c8129d0818098c74062d44d966bd73c02132957b
Preadator <nrish.ns@gmail.com>
2025-05-23 20:48:30 +0545

    first commit

---
+++ src/hostello/Hostello.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hostello;
+import hostello.view.LoginView;
+
+/**
+ *
+ * @author user
+ */
+public class Hostello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginView loginView = new LoginView();
+        loginView.setVisible(true);
+    }
+    
+}
