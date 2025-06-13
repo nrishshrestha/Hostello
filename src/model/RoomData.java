8be86f9e30324889235455f260e44d03fc809fb7
hridesh <hrideshrai0001@gmail.com>
2025-06-13 20:42:04 +0545

    Empty Constructor made

--- src/model/RoomData.java
+++ src/model/RoomData.java
@@ -4,68 +4,15 @@
  */
 package model;
 
-/**
- *
- * @author Acer
- */
 public class RoomData {
+    private int room_id;
+    private String room_no;
+    private String room_type;
+    private double room_cost;
+    private String room_status;
+    private int user_id; // warden_id
     
-    private int roomId;
-    private String roomNo;
-    private String roomType;
-    private Float roomCost;
-    private String roomStatus;
-    private int userId;
+     public RoomData() {}
+
     
-    public RoomData(String roomNo, String roomType, Float roomCost, String roomStatus, int userId){
-  
-        this.roomNo = roomNo;
-        this.roomType = roomType;
-        this.roomCost = roomCost;
-        this.roomStatus = roomStatus;
-        this.userId = userId;
-    }
-    
-    // Getters and Setters
-    public int getRoomId() {
-        return roomId;
-    }
-    public void setRoomId(int roomId) {
-        this.roomId = roomId;
-    }
-    
-    public String getRoomNo() {
-        return roomNo;
-    }
-    public void setRoomNo(String roomNo) {
-        this.roomNo = roomNo;
-    }
-    
-    public String getRoomType() {
-        return roomType;
-    }
-    public void setRoomType(String roomType) {
-        this.roomType = roomType;
-    }
-    
-    public Float getRoomCost() {
-        return roomCost;
-    }
-    public void setRoomCost(Float roomCost) {
-        this.roomCost = roomCost;
-    }
-    
-    public String getRoomStatus() {
-        return roomStatus;
-    }
-    public void setRoomStatus(String roomStatus) {
-        this.roomStatus = roomStatus;
-    }
-    
-    public int getUserId() {
-        return userId;
-    }
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
 }
